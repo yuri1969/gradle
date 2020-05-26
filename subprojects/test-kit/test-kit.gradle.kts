96b3b4d95627656b081733204e62b331f3efd584
Jendrik Johannes <jendrik@gradle.com>
2020-05-26 11:10:56 +0200

    Mark projects as 'plugins' that are shipped as 'plugins' in distribution

--- subprojects/test-kit/test-kit.gradle.kts
+++ subprojects/test-kit/test-kit.gradle.kts
@@ -16,7 +16,7 @@
 import org.gradle.gradlebuild.testing.integrationtests.cleanup.WhenNotEmpty
 
 plugins {
-    gradlebuild.distribution.`core-api-java`
+    gradlebuild.distribution.`plugins-api-java`
 }
 
 dependencies {
